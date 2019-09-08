1edad57e1012e6f493bc8f84cfcebc176dc07b8d
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2019-09-08 09:30:29 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  
+  function startGame(){
+    
+    
+  }
+  $("#Xselected").click(function(){
+    $("#chooseXO").fadeOut(1000, startGame());
+  });
+  
+//  on("click", function (){
+//    $("#tictactoe").fadeOut(1000);
+//  });
+
+
+
+  
+  
+}); //end document ready
